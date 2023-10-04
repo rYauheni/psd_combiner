b37c551d27e0b3a8efdf4708cfde15bb1f0fff92
rYauheni <ra.yauheni@gmail.com>
2023-10-04 17:45:54 +0300

    added attribute in ParseTemplate class

--- parse_templates/parse_template.py
+++ parse_templates/parse_template.py
@@ -1,9 +1,10 @@
 class ParseTemplate:
-    def __init__(self, title, detector, start, end):
+    def __init__(self, title, detector, start, end, required=True):
         self.title = title
         self.detector = detector
         self.start = start
         self.end = end
+        self.required = required
 
     def __repr__(self):
         return f'ParseTemplate object <{self.title}>'
