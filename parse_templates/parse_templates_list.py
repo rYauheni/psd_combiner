b8e541ceada814009291491fc02ddb1c3688bd64
rYauheni <ra.yauheni@gmail.com>
2023-10-04 17:47:42 +0300

    added template 're_entry', added attribute 'required' to all templates

--- parse_templates/parse_templates_list.py
+++ parse_templates/parse_templates_list.py
@@ -1,17 +1,27 @@
 from parse_templates.parse_template import ParseTemplate
 
-buy_in_template = ParseTemplate(
-    title='buy_in',
-    detector=r'Tournament #\d+',
-    start=r'[$¥€]',
-    end=r' '
-)
+PARSE_TEMPLATES = (
+    ParseTemplate(
+        title='buy_in',
+        detector=r'Tournament #\d+',
+        start=r'[$¥€]',
+        end=r' ',
+        required=True
+    ),
 
-total_received_template = ParseTemplate(
-    title='total_received',
-    detector='received a total',
-    start=r'[$¥€]',
-    end=r'\.\n'
-)
+    ParseTemplate(
+        title='total_received',
+        detector=r'received a total',
+        start=r'[$¥€]',
+        end=r'\.\n',
+        required=True
+    ),
 
-PARSE_TEMPLATES = (buy_in_template, total_received_template)
+    ParseTemplate(
+        title='re_entry',
+        detector=r're-entries',
+        start=r'made ',
+        end=r' ',
+        required=False
+    )
+)
