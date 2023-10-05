5a316449227c8a881c38f0fba755f649c0aa0fe3
rYauheni <ra.yauheni@gmail.com>
2023-10-05 18:28:35 +0300

    added attribute ttype in ParseTemplate class

--- parse_templates/parse_template.py
+++ parse_templates/parse_template.py
@@ -1,10 +1,11 @@
 class ParseTemplate:
-    def __init__(self, title, detector, start, end, required=True):
+    def __init__(self, title, detector, start, end, required=True, ttype=str):
         self.title = title
         self.detector = detector
         self.start = start
         self.end = end
         self.required = required
+        self.ttype = ttype
 
     def __repr__(self):
         return f'ParseTemplate object <{self.title}>'
