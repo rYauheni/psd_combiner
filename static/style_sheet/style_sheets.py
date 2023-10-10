b2c4d3873f16389bc4511524beba5bc0ccebef7f
rYauheni <ra.yauheni@gmail.com>
2023-10-10 17:15:00 +0300

    added static files into the project

---
+++ static/style_sheet/style_sheets.py
@@ -0,0 +1,21 @@
+RESULT_SS = """
+            height: 70px;
+            border: 4px solid #1B8366;
+            background-color: white;
+            border-radius: 20px;
+            padding-left: 10px;
+            color: #333333;
+            font-family: Roboto, sans-serif;
+            font-size: 16px;
+            font-weight: bold;
+        """
+
+ERRORS_LOG_SS = """
+            border: 4px solid #1B8366;
+            background-color: white;
+            border-radius: 20px;
+            padding-left: 10px;
+            color: #333333;
+            font-family: Roboto, sans-serif;
+            font-size: 12px;
+        """
