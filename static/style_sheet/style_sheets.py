fc025b7b5ec8abf269bda257390763916c3495bf
rYauheni <ra.yauheni@gmail.com>
2023-10-11 17:44:59 +0300

    added stylesheets

--- static/style_sheet/style_sheets.py
+++ static/style_sheet/style_sheets.py
@@ -19,3 +19,29 @@ ERRORS_LOG_SS = """
             font-family: Roboto, sans-serif;
             font-size: 12px;
         """
+
+BUTTON_SS = """
+            QPushButton {
+            padding: 12px 20px;
+            box-sizing: border-box;
+            border: 4px solid #1B8366;
+            border-radius: 20px;
+            background-color: #3BB08F;
+            color: white;
+            font-size: 16px;
+            font-weight: bold;
+            cursor: pointer;
+            margin-left: auto;
+            max-width: 60%;
+            }
+            QPushButton:pressed {
+               background-color: #1B8366;
+            }
+        """
+
+SUBSIDIARY_TEXT_SS = """
+            color: white;
+            font-family: Roboto, sans-serif;
+            font-size: 16px;
+            font-weight: bold;
+        """
